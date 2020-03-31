18d0e62820b392800f0732042877f0397ae4970e
Dan McCreary <dan.mccreary@gmail.com>
2020-03-31 12:22:47 -0500

    Added gradle scripts to load a schema

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,49 @@
+import com.optum.giraffle.tasks.GsqlTask
+
 plugins {
     id("com.optum.giraffle") version "1.3.3"
+    id("net.saliman.properties") version "1.5.1"
 }
 
 repositories {
     jcenter()
+}
+
+val gsqlGraphname: String by project // <1>
+val gsqlHost: String by project
+val gsqlUserName: String by project
+val gsqlPassword: String by project
+val gsqlAdminUserName: String by project
+val gsqlAdminPassword: String by project
+val tokenMap: LinkedHashMap<String, String> =
+    linkedMapOf("graphname" to gsqlGraphname) // <2>
+
+val grpSchema: String = "Tigergraph Schema"
+
+tigergraph { // <3>
+    scriptDir.set(file("db_scripts"))
+    tokens.set(tokenMap)
+    serverName.set(gsqlHost)
+    userName.set(gsqlUserName)
+    password.set(gsqlPassword)
+    adminUserName.set(gsqlAdminUserName)
+    adminPassword.set(gsqlAdminPassword)
+}
+
+tasks {
+    val createSchema by registering(GsqlTask::class) { // <4>
+        group = grpSchema
+        description = "Create the schema on the database"
+        dependsOn("dropSchema") // <5>
+        scriptPath = "schema.gsql" // <6>
+        superUser = true // <7>
+    }
+
+    val dropSchema by registering(GsqlTask::class) {
+        group = grpSchema
+        description = "Drops the schema on the database"
+        scriptPath = "drop.gsql"
+        superUser = true
+    }
 }
 
